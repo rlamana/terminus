af131f1bfd3dab481754c8301f18c364cafa6515
Ramón Lamana <rlamana@gmail.com>
2016-11-07 21:32:27 +0100

    Remove old dependency

---
+++ tests/main.js
@@ -0,0 +1,26 @@
+var tests = [];
+for (var file in window.__karma__.files) {
+    if (window.__karma__.files.hasOwnProperty(file)) {
+        if (/tests\/test-.*js$/.test(file)) {
+          tests.push(file);
+          console.log(file);
+        }
+    }
+}
+
+requirejs.config({
+    // Karma serves files from '/base'
+    baseUrl: '/base/src',
+
+    paths: {
+    },
+
+    shim: {
+    },
+
+    // ask Require.js to load these files (all our tests)
+    deps: tests,
+
+    // start test run, once Require.js is done
+    callback: window.__karma__.start
+});
